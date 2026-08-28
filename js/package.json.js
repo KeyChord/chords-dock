172722603892e042abb2b85d247b61385bc2ba06
Leon Si <leon@leonsilicon.com>
2026-08-28 21:24:18 +0000

    fmt: {}
    Committed via a GitHub action: https://github.com/KeyChord/chords/actions/runs/33212361686
    
    Co-authored-by: leonsilicon <leonsilicon@users.noreply.github.com>

--- js/package.json.js
+++ js/package.json.js
@@ -1,10 +1,8 @@
 //#region package.json
 var name = "@keychord/chords-dock";
 var type = "module";
-var dependencies = { "jxa-run-compat": "latest" };
 var devDependencies = {
-	"@jxa/global-type": "latest",
-	"@keychord/chords-menu": "github:KeyChord/chords-menu",
+	"@keychord/chords-menu": "workspace:*",
 	"@keychord/config": "catalog:",
 	"@keychord/tsconfig": "catalog:"
 };
@@ -12,9 +10,8 @@ var packageManager = "pnpm@10.33.0";
 var package_default = {
 	name,
 	type,
-	dependencies,
 	devDependencies,
 	packageManager
 };
 //#endregion
-export { package_default as default, dependencies, devDependencies, name, packageManager, type };
+export { package_default as default, devDependencies, name, packageManager, type };
